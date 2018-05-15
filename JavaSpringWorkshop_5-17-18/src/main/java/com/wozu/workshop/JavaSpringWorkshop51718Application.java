5ce8fb29e2200125644f8dc7bc00d10ee2e099bc
Steve Bishop <xipooo@gmail.com>
2018-05-15 16:19:20 -0700

    Initial Commit

---
+++ JavaSpringWorkshop_5-17-18/src/main/java/com/wozu/workshop/JavaSpringWorkshop51718Application.java
@@ -0,0 +1,12 @@
+package com.wozu.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWorkshop51718Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWorkshop51718Application.class, args);
+	}
+}
